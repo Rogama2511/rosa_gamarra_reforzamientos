75fb4e607a825753c3191a39461c6eb319dc7a8a
Rosa Gamarra <rgamaguirre@gmail.com>
2025-03-30 15:56:11 -0500

    files

--- Reforzamiento 04/test_dic_9.py
+++ Reforzamiento 04/test_dic_9.py
@@ -12,4 +12,33 @@ ya está cancelada” caso contrario “El número de factura no existe”
 Considerar que cada vez que se realice algún pago o ingreso de una nueva
 factura se mostrará inmediatamente al diccionario actualizado. """
 
-#
+#Diccionario facturas
+dicc_factur = {
+    "03054": 250.75,
+    "00123": 480.50,
+    "10289": 320.00,
+    "50345": 150.25 }
+
+#Ingresar opcion a realizar facturas
+print("Ingrese que desea hacer: (a) Factura nueva (b) Pagar factura")
+
+opcion = input("Ingresa la opcion: ")
+if opcion == "a":
+    num_factura = input("Ingrese número de factura: ")
+    if num_factura in dicc_factur:
+        print("El número de factura ya existe")
+    else:
+        costo = float(input("Ingrese costo: "))
+        dicc_factur[num_factura] = costo
+        print("Factura agregada")
+elif opcion == "b":
+    num_factura = input("Ingrese factura a cancelar:")
+    if num_factura in dicc_factur:
+        print("Factura cancelada")
+    else:
+        print("Factura no existe")
+else:
+    print("Opcion no valida")
+
+#Mostrar facturas
+print("El diccionario de facturas es: {}".format(dicc_factur))
